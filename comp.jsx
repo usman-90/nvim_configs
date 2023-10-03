f936f2ca37c2a780b13f2e204679f6d66521683f
Usman Siddiqui <siddiquiusman328@gmail.com>
2023-10-03 10:44:48 +0500

    ..

---
+++ comp.jsx
@@ -0,0 +1,12 @@
+const Product = () => {
+        	return (
+ 		<div>
+ 			<p className="yes" onClick={(e) => {
+ 				console.log("Hello worlds")
+ 			}}>
+ 			</p>
+ 		</div>
+ 	)
+}
+export default Product;
+
---
+++ exercise.js
@@ -0,0 +1,29 @@
+const a = {
+    usman : 70,
+    noman : 80,
+    age  :[2,3,5]
+}
+function helloWorld (){
+	console.log("Hello world!")
+
+}
+helloWorld()
+
+const date = new Date()
+console.log(date)
+console.log("Yes now its looking so good! ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
